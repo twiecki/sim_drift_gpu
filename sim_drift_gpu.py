cdd3681f09ab32eb377733a1d5cbd507ea4c331b
Thomas Wiecki <thomas.wiecki@gmail.com>
2011-12-09 13:13:21 -0500

    Cleaned up interface.

--- sim_drift_gpu.py
+++ sim_drift_gpu.py
@@ -90,37 +90,59 @@ code = """
 
 mod = SourceModule(code, keep=False, no_extern_c=True)
 
-size = 512
-seed = 1234
+_size = 128
 
-sim_drift = mod.get_function("sim_drift")
-sim_drift_var_thresh = mod.get_function("sim_drift_var_thresh")
+_sim_drift_cuda = mod.get_function("sim_drift")
+_sim_drift_var_thresh_cuda = mod.get_function("sim_drift_var_thresh")
 
-g = pycuda.curandom.XORWOWRandomNumberGenerator()
+_generator = None
+_thresh = []
+_thresh_gpu = None
+_out = None
+
+def sim_drift(v, V, a, z, Z, t, T, size, dt=1e-4, update=False):
+    global _generator, _out, _size
+    if size != size:
+        update = True
+    if _generator is None or update:
+        _generator = pycuda.curandom.XORWOWRandomNumberGenerator()
+    if _out is None or update:
+        _out = gpuarray.empty(size, dtype=np.float32)
+
+    _sim_drift_cuda(_generator.state, np.float32(v), np.float32(V), np.float32(a), np.float32(z), np.float32(Z), np.float32(t), np.float32(T), np.float32(dt), np.float32(1), _out, block=(64,1,1), grid=(size//64+1,1))
+
+    return _out.get()
+
+def sim_drift_var_thresh(v, V, a, z, Z, t, T, max_time, size, dt=1e-4, update=False):
+    global _generator, _thresh, _thresh_gpu, _out
+
+    # Init
+    if size != _size:
+        update = True
+    if _generator is None or update:
+        _generator = pycuda.curandom.XORWOWRandomNumberGenerator()
+    if _thresh_gpu is None or update:
+        _thresh_gpu = gpuarray.to_gpu(a)
+    if np.any(_thresh != a) or update:
+        # Threshold function changed from before
+        _thresh_gpu = gpuarray.to_gpu(a)
+        _thresh = a
+    if _out is None or update:
+        _out = gpuarray.empty(size, dtype=np.float32)
+
+    _sim_drift_var_thresh_cuda(_generator.state, np.float32(v), np.float32(V), _thresh_gpu, np.float32(z), np.float32(Z), np.float32(t), np.float32(T), np.float32(dt), np.float32(1), np.float32(max_time), _out, block=(64,1,1), grid=(size//64+1,1))
+
+    return _out.get()
 
 dt = 1e-4
 max_time = 5.
 steps = max_time/dt
 a = 2
-thresh = np.array(a*np.exp(-np.linspace(0, max_time, steps)), dtype=np.float32)
-thresh_gpu = gpuarray.to_gpu(thresh)
+rate = .5
+thresh_func = np.array(a*np.exp(-rate*np.linspace(0, max_time, steps)), dtype=np.float32)
+size = 128
+print sim_drift_var_thresh(0, .1, thresh_func, 0., .1, .3, .1, max_time, size)
 
-out = gpuarray.empty(size, dtype=np.float32)
+print sim_drift(1, .1, 2, .5, .1, .3, .1, size)
 
-def sim_drift_gpu(out, v, V, a, z, Z, t, T, size, dt=1e-4):
-    sim_drift(g.state, np.float32(v), np.float32(V), np.float32(a), np.float32(z), np.float32(Z), np.float32(t), np.float32(T), np.float32(dt), np.float32(1), out, block=(64,1,1), grid=(size//64+1,1))
-
-def sim_drift_var_thresh_gpu(out, v, V, a, z, Z, t, T, size, dt=1e-4):
-    sim_drift_var_thresh(g.state, np.float32(v), np.float32(V), thresh_gpu, np.float32(z), np.float32(Z), np.float32(t), np.float32(T), np.float32(dt), np.float32(1), np.float32(max_time), out, block=(64,1,1), grid=(size//64+1,1))
-
-sim_drift_var_thresh_gpu(out, 1, .1, thresh_gpu, 0., .1, .3, .1, size)
-print out.get()
-
-sim_drift_gpu(out, 1, .1, 2, .5, .1, .3, .1, size)
-print "Out:"
-print out.get()
-
-sim_drift_gpu(out, 1, .1, 2, .5, .1, .3, .1, size)
-
-print "Out:"
-print out.get()
+print sim_drift(1, .1, 2, .5, .1, .3, .1, size)
